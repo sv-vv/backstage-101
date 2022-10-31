6fd7e606fa4321fb11883e0376c2ff8b7f7e912e
Cristian Hurdubai <cristian.hurdubai@softvision.com>
2022-10-31 10:07:29 +0200

    added sample plugin back

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -37,6 +37,7 @@ import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/
 import { githubAuthApiRef } from '@backstage/core-plugin-api';
 import { SignInPage } from '@backstage/core-components';
 import { FirstPage } from '@internal/plugin-first';
+import { SecondaryPage } from '@internal/plugin-secondary';
 
 const app = createApp({
   apis,
@@ -110,6 +111,7 @@ const routes = (
     <Route path="/settings" element={<UserSettingsPage />} />
     <Route path="/catalog-graph" element={<CatalogGraphPage />} />
     <Route path="/first" element={<FirstPage />}/>
+    <Route path="/secondary" element={<SecondaryPage />}/>
   </FlatRoutes>
 );
 
---
+++ plugins/secondary/src/index.ts
@@ -0,0 +1,1 @@
+export { secondaryPlugin, SecondaryPage } from './plugin';
